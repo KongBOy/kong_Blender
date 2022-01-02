db29537afa523795b2d146728784c81274331725
KongBoy <s89334roy@yahoo.com.tw>
2022-01-02 10:33:32 +0800

    加入 wc_add_mask

--- step2a_distribute_to_dir.py
+++ step2a_distribute_to_dir.py
@@ -4,6 +4,18 @@ sys.path.append("../kong_util")
 from util import get_dir_certain_file_names
 from build_dataset_combine import Check_dir_exist_and_build
 import shutil
+from step0_disk_index import render_out_dir
+
+image_ord_dir     = render_out_dir
+uv_ord_dir        = render_out_dir
+uv_visual_ord_dir = render_out_dir
+wc_ord_dir        = render_out_dir
+wc_visual_ord_dir = render_out_dir
+image_dst_dir     = render_out_dir + "/" + "0_image"
+uv_dst_dir        = render_out_dir + "/" + "1_uv"
+uv_visual_dst_dir = render_out_dir + "/" + "1_uv_visual"
+wc_dst_dir        = render_out_dir + "/" + "2_wc"
+wc_visual_dst_dir = render_out_dir + "/" + "2_wc_visual"
 
 def grab_ord_dir_certain_file_to_dst_dir(ord_dir, certain_word, certain_ext, dst_dir, print_msg=False):
     Check_dir_exist_and_build(dst_dir)  ### 建立 dst_dir， 不可以用 build_new_dir 喔！ 要不然執行第二次以上 dst_dir 的東西就會被刪掉
@@ -16,28 +28,12 @@ def grab_ord_dir_certain_file_to_dst_dir(ord_dir, certain_word, certain_ext, dst
         if(print_msg): print(ord_path, "->", dst_path, "finish~")
 
 def blender_render_result_split_to_dir(render_out_dir):
-    image_ord_dir = render_out_dir
-    image_dst_dir = render_out_dir + "/" + "0_image"
-    grab_ord_dir_certain_file_to_dst_dir(ord_dir=image_ord_dir, certain_word="0_image", certain_ext=".png", dst_dir=image_dst_dir, print_msg=True)
-
-    uv_ord_dir = render_out_dir
-    uv_dst_dir = render_out_dir + "/" + "1_uv"
-    grab_ord_dir_certain_file_to_dst_dir(ord_dir=uv_ord_dir, certain_word="1_uv", certain_ext=".exr", dst_dir=uv_dst_dir, print_msg=True)
-
-    uv_visual_ord_dir = render_out_dir
-    uv_visual_dst_dir = render_out_dir + "/" + "1_uv_visual"
+    grab_ord_dir_certain_file_to_dst_dir(ord_dir=image_ord_dir,     certain_word="0_image",    certain_ext=".png", dst_dir=image_dst_dir,     print_msg=True)
+    grab_ord_dir_certain_file_to_dst_dir(ord_dir=uv_ord_dir,        certain_word="1_uv",       certain_ext=".exr", dst_dir=uv_dst_dir,        print_msg=True)
     grab_ord_dir_certain_file_to_dst_dir(ord_dir=uv_visual_ord_dir, certain_word="1_uv_image", certain_ext=".png", dst_dir=uv_visual_dst_dir, print_msg=True)
-
-    wc_ord_dir = render_out_dir
-    wc_dst_dir = render_out_dir + "/" + "2_wc"
-    grab_ord_dir_certain_file_to_dst_dir(ord_dir=wc_ord_dir, certain_word="2_wc", certain_ext=".exr", dst_dir=wc_dst_dir, print_msg=True)
-
-    wc_visual_ord_dir = render_out_dir
-    wc_visual_dst_dir = render_out_dir + "/" + "2_wc_visual"
+    grab_ord_dir_certain_file_to_dst_dir(ord_dir=wc_ord_dir,        certain_word="2_wc",       certain_ext=".exr", dst_dir=wc_dst_dir,        print_msg=True)
     grab_ord_dir_certain_file_to_dst_dir(ord_dir=wc_visual_ord_dir, certain_word="2_wc_image", certain_ext=".png", dst_dir=wc_visual_dst_dir, print_msg=True)
 
 
 if(__name__ == "__main__"):
-    from step0_disk_index import render_out_dir
-
     blender_render_result_split_to_dir(render_out_dir)
--- step3_exr_to_knpy.py
+++ step3_exr_to_knpy.py
@@ -1,34 +1,66 @@
 import sys
-sys.path.append("../kong_util")
+sys.path.append("kong_util")
+from step0_disk_index import render_out_dir
+from build_dataset_combine import Save_exr_as_npy2, Save_npy_dir_as_knpy, Check_dir_exist_and_build
 
-from build_dataset_combine import Save_exr_as_npy2, Save_npy_dir_as_knpy
-import time
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 def dir_exr_to_npy_to_knpy(exr_ord_dir, npy_dst_dir, knpy_dst_dir):
     Save_exr_as_npy2(exr_ord_dir, npy_dst_dir)
     Save_npy_dir_as_knpy(npy_dst_dir, knpy_dst_dir)
 
 if(__name__ == "__main__"):
-    # from step0_disk_index import render_out_dir
 
-    # uv_name = "1_uv"
-    # uv_exr_ord_dir  = render_out_dir + "/" + uv_name
-    # uv_npy_dst_dir  = render_out_dir + "/" + uv_name + "_npy"
-    # uv_knpy_dst_dir = render_out_dir + "/" + uv_name + "_knpy"
+    '''exr -> npy -> knpy'''
+    uv_name = "1_uv"
+    uv_exr_ord_dir  = render_out_dir + "/" + uv_name
+    uv_npy_dst_dir  = render_out_dir + "/" + uv_name + "_npy"
+    uv_knpy_dst_dir = render_out_dir + "/" + uv_name + "_knpy"
     # dir_exr_to_npy_to_knpy(uv_exr_ord_dir, uv_npy_dst_dir, uv_knpy_dst_dir)
 
-    # wc_name = "2_wc"
-    # wc_exr_ord_dir  = render_out_dir + "/" + wc_name
-    # wc_npy_dst_dir  = render_out_dir + "/" + wc_name + "_npy"
-    # wc_knpy_dst_dir = render_out_dir + "/" + wc_name + "_knpy"
-    # dir_exr_to_npy_to_knpy(wc_exr_ord_dir, wc_npy_dst_dir, wc_knpy_dst_dir)
+    '''exr -> npy -> add_M -> knpy'''
+    wc_name = "2_wc"
+    wc_exr_ord_dir  = f"{render_out_dir}/{wc_name}"
+    wc_npy_dst_dir  = f"{render_out_dir}/{wc_name}_npy"  ### exr   -> npy
+    wc_w_M_dst_dir  = f"{render_out_dir}/{wc_name}_w_M_npy"  ### npy   -> add_M 
+    wc_knpy_dst_dir = f"{render_out_dir}/{wc_name}_knpy" ### add_M -> knpy
+    # Save_exr_as_npy2(wc_exr_ord_dir, wc_npy_dst_dir)  ### exr -> npy
 
-    uv_name = "1_uv"
-    uv_npy_dst_dir  = "H:/Doc3D" + "/" + uv_name + "_npy"
-    uv_knpy_dst_dir = "H:/Doc3D" + "/" + uv_name + "_knpy"
+    Check_dir_exist_and_build(wc_w_M_dst_dir)
+    for uv_filename, wc_filename in tqdm(zip( os.listdir(uv_npy_dst_dir), os.listdir(wc_npy_dst_dir) ) ):
+        uv_path = f"{uv_npy_dst_dir}/{uv_filename}"
+        wc_path = f"{wc_npy_dst_dir}/{wc_filename}"
+        uv = np.load(uv_path)
+        wc = np.load(wc_path)
+        mask = uv[..., 0]
+        wc[..., 3] = mask
+
+        ### 視覺化一下看看有沒有寫錯
+        # fig, ax = plt.subplots(1, 4, figsize=(20, 5))
+        # ax[0].imshow(wc[..., 0])
+        # ax[1].imshow(wc[..., 1])
+        # ax[2].imshow(wc[..., 2])
+        # ax[3].imshow(wc[..., 3])
+        # plt.show()
+        # print(wc.shape)
+
+        dst_path = f"{wc_w_M_dst_dir}/{wc_filename}"
+        np.save(dst_path, wc)
+
+
+    Save_npy_dir_as_knpy(wc_w_M_dst_dir, wc_knpy_dst_dir)
+
+
+
+    # uv_name = "1_uv"
+    # uv_npy_dst_dir  = "H:/Doc3D" + "/" + uv_name + "_npy"
+    # uv_knpy_dst_dir = "H:/Doc3D" + "/" + uv_name + "_knpy"
     # for i in range(1, 21 + 1):
     #     start_time = time.time()
     #     uv_exr_ord_dir  = f"J:/swat3D/uv/{i}"
     #     Save_exr_as_npy2(uv_exr_ord_dir, uv_npy_dst_dir)
     #     print("one dir Save_exr_as_npy2 cost time:", time.time() - start_time)
-    Save_npy_dir_as_knpy(uv_npy_dst_dir, uv_knpy_dst_dir, core_amount=30)
+    # Save_npy_dir_as_knpy(uv_npy_dst_dir, uv_knpy_dst_dir, core_amount=30)
